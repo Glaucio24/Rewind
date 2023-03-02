620e36c4051e633d8c6676482f681bdf0a399d80
Glaucio Alexandre <108555972+Glaucio24@users.noreply.github.com>
2023-03-02 17:49:43 -0600

    added  enter keycode

--- js/site.js
+++ js/site.js
@@ -1,24 +1,44 @@
 //Get the string 
-//controlller function
+//controller function
 function getSstring() {
 
-    document.getElementById("alert").classList.add("invisible");
+    // document.getElementById("alert").classList.add("invisible");
  
     let userString = document.getElementById("userInput").value;
 
+    let inputTag=document.getElementById("userInput");
+
     let reverse = reverseString(userString);//declared a function into a variable 
 
+    
+
     displayString(reverse);
+    submit(inputTag);
 }
+
 //Reverse the string 
 //logic function
 function reverseString(userString) {
-    let reverse = [];   
+    let reverse = ''; //string or array to store reversed string  
 
     for (let index = userString.length - 1; index >= 0; index--) {
         reverse += userString[index];
     }
     return reverse;
+
+}
+//Submittion button
+function submit(inputTag){
+    console.log(inputTag);
+
+    inputTag.addEventListener("keyup", function(event){
+        if(event.keyCode === 13){
+            event.preventDefault();
+            displayString(reverseString(inputTag.value));
+        }       
+    });
+        
+
 }
 
 //Display the reversed string to the user//view function
@@ -27,4 +47,6 @@ function displayString(reverse) {
 
     document.getElementById("msg").innerHTML = `Your string reversed is: ${reverse}`; //Template literals 
     document.getElementById("alert").classList.remove("invisible");
+
 }
+
